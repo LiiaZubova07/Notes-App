10353a1e86284ec0a132f3f755a5184654e8278d
Liia Zubova <vardlitmo@gmail.com>
2022-11-18 19:23:56 +0300

    add: buttons js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const addBtn = document.querySelector('.note-add');
 
 //1 - написать  функцию createNote
 //2 - добавить кнопку и обработчик на добавление карточки
+//3 - добавить стили
+//4 - реалтзовать редактирование
 
 function createNote(title, text) {
   const noteEl = document.createElement('div');
@@ -10,25 +12,40 @@ function createNote(title, text) {
   noteEl.innerHTML = `
 	<div class = 'note-header'>
 <p id = "note-title">${title}</p>
-<textarea id = 'note-textarea' class= 'hidden'>${text}</textarea>
+<textarea id = 'note-title-input' class= 'hidden'>${title}</textarea>
 <div>
 <button class ='note-edit'><i class="fa-solid fa-pen-to-square"></i></button>
 <button class ='note-delete'><i class="fa-solid fa-trash"></i></button>
 </div>
 	</div>
-	
 	<textarea id = 'note-textarea' class= 'hidden'>${text}</textarea>`;
-
-	
 
   const editBtn = noteEl.querySelector('.note-edit');
   const deleteBtn = noteEl.querySelector('.note-delete');
+  const titleEl = noteEl.querySelector('#note-title');
+  const textEl = noteEl.querySelector('#note-text');
+  const titleInputEl = noteEl.querySelector('#note-title-input');
+  const textInputEl = noteEl.querySelector('#note-textarea');
+
 
   editBtn.addEventListener('click', (e) => {
+	titleEl.classList.toggle('hidden');
+	textEl.classList.toggle('hidden');
 
+	titleInputEl.classList.toggle('hidden');
+	textInputEl.classList.toggle('hidden');
   });
+
   deleteBtn.addEventListener('click', (e) => {
-noteEl.remove();
+    noteEl.remove();
+  });
+
+  titleInputEl.addEventListener('input', (e) => {
+titleEl.innerText = e.target.value;
+  });
+
+  textInputEl.addEventListener('input', (e) => {
+textEl.innerText = e.target.value;
   });
 
   return noteEl;
